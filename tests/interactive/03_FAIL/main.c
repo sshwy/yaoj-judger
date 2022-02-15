9c04adcbc79dcac5f4bdd57d3282387f8c2696db
sshwy <jy.cat@qq.com>
2022-02-15 17:20:29 +0800

    test: add tests for interactive

---
+++ tests/interactive/03_FAIL/main.c
@@ -0,0 +1,28 @@
+// a.cpp
+#include <assert.h>
+#include <stdio.h>
+
+int check(int x) {
+  printf("? %d\n", x); // check if ? >= x
+  fflush(stdout);
+  fprintf(stderr, "? %d\n", x); // check if ? >= x
+  assert(scanf("%d", &x) != EOF);
+  return x == 1;
+}
+int main() {
+  // sleep(1);
+  int n;
+  assert(scanf("%d", &n) != EOF);
+  fprintf(stderr, "a.cpp: n %d\n", n);
+  int l = 1, r = n, mid;
+  while (l < r) {
+    mid = (l + r + 1) >> 1;
+    if (check(mid))
+      l = mid;
+    else
+      r = mid - 1;
+  }
+  printf("! %d\n", l);
+  fprintf(stderr, "! %d\n", l);
+  return 0;
+}
