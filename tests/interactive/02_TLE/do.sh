a5555ad2903193ac0db18b3f14f46da796c48cf1
sshwy <jy.cat@qq.com>
2022-02-15 11:06:57 +0800

    test: add test for interactive

--- tests/interactive/02_TLE/do.sh
+++ tests/interactive/02_TLE/do.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+rm -f interactor.out interactor.err main.err
 
 cc main.c -o main.local
 cc interactor.c -o interactor.local
 touch interactor.out interactor.err main.err
+
 ../../../judger_interactive.local main.local interactor.local \
   interactor.in interactor.out interactor.err main.err \
   -r TLE \
@@ -10,10 +12,10 @@ touch interactor.out interactor.err main.err
   -P ../../../policy \
   -p free \
   -t 500
-cat .log.local
-echo -e "\033[32m[interactor.out]\033[0m"
-cat interactor.out
-echo -e "\033[32m[interactor.err]\033[0m"
-cat interactor.err
-echo -e "\033[32m[main.err]\033[0m"
-cat main.err
+# cat .log.local
+# echo -e "\033[32m[interactor.out]\033[0m"
+# cat interactor.out
+# echo -e "\033[32m[interactor.err]\033[0m"
+# cat interactor.err
+# echo -e "\033[32m[main.err]\033[0m"
+# cat main.err
