5245d6b81378ee29314ac02b8f449c22389d00b8
sshwy <jy.cat@qq.com>
2022-02-16 12:04:38 +0800

    test: reduce verbose

--- tests/traditional/test.mk
+++ tests/traditional/test.mk
@@ -2,16 +2,16 @@ SHELL=/bin/sh
 .PHONY=test compile clean
 
 test: compile
-	$(RM) main.out main.err
-	touch main.out main.err
-	$(TESTER) main.local main.in main.out main.err $(IO) \
+	@$(RM) main.out main.err
+	@touch main.out main.err
+	@$(TESTER) main.local main.in main.out main.err $(IO) \
 		-r $(R_CODE) --log=.log.local \
 		-P $(PROJECT_ROOT)policy -p c_std_io_coverage \
 		-t $(TIME) -g $(OUTPUT) --virmem=$(V_MEM) \
 		--actmem=$(A_MEM) --stkmem=$(S_MEM) \
 
 compile:
-	$(CC) main.c -o main.local $(EXTRA_FLAG)
+	@$(CC) main.c -o main.local $(EXTRA_FLAG)
 
 clean:
 	$(RM) main.local .log.local main.out main.err
