54159734fe627f3d8108649a96f3bebf85a6aec0
sshwy <jy.cat@qq.com>
2022-04-04 13:38:04 +0800

    test: remove some temporary files

--- tests/test.sh
+++ tests/test.sh
@@ -14,34 +14,34 @@ compile_c asserts/prlimit.c prlimit.local
 compile_c asserts/03_tle.c 03_tle.local
 compile_cpp asserts/stress.cpp stress.local
 
-touch t.out t.err t2.err t.in
+touch t.out t.in
 rm -rf .log
 mkdir .log
 
 declare_test
-run_general t.in t.out t.err asserts/ls.sh -r OK -P asserts -p free -t 1000
+run_general t.in t.out /dev/null asserts/ls.sh -r OK -P asserts -p free -t 1000
 
 declare_test
-run_general t.in t.out t.err asserts/ls.sh -r OK -p builtin:free -t 1000 -m 128
+run_general t.in t.out /dev/null asserts/ls.sh -r OK -p builtin:free -t 1000 -m 128
 
 declare_test
-run_general t.in t.out t.err asserts/ls.sh -p builtin:free -t 1000 -m 128 --json
+run_general t.in t.out /dev/null asserts/ls.sh -p builtin:free -t 1000 -m 128 --json
 
 declare_test
-run_traditional aplusb.local asserts/aplusb.in t.out t.err\
+run_traditional aplusb.local asserts/aplusb.in t.out /dev/null\
   -r SE -p builtin:free -t 1000 -m 128
 
 declare_test # 5
-run_traditional aplusb.local m.in t.out t.err std\
+run_traditional aplusb.local m.in t.out /dev/null std\
   -r SE -p builtin:free -t 1000 -m 128
 
 declare_test
 
-run_traditional aplusb.local asserts/aplusb.in m.out t.err std\
+run_traditional aplusb.local asserts/aplusb.in m.out /dev/null std\
   -r SE -p builtin:free -t 1000 -m 128
 
 declare_test
-run_traditional aplusb.local asserts/aplusb.in t.out t.err s\
+run_traditional aplusb.local asserts/aplusb.in t.out /dev/null s\
   -r SE -p builtin:free -t 1000 -m 128
 
 declare_test
@@ -49,121 +49,117 @@ run_traditional aplusb.local asserts/aplusb.in t.out m.err std\
   -r SE -p builtin:free -t 1000 -m 128
 
 declare_test
-run_traditional aplusb.local asserts/aplusb.in t.out t.err std\
+run_traditional aplusb.local asserts/aplusb.in t.out /dev/null std\
   -r SE -p builtin:free -t 1000 -m -128
 
 declare_test # 10
-run_traditional aplusb.local asserts/aplusb.in t.err t.err std\
-  -r SE -p builtin:free -t 1000
+run_general asserts/aplusb.in t.out /dev/null 03_tle.local \
+	-r TLE -m 30 -t 1000 -g 64 -p builtin:_coverage_general_strict
 
 declare_test
-run_general t.in t.out t.err asserts/ls.sh -r SE -p builtin:free -t -1
+run_general t.in t.out /dev/null asserts/ls.sh -r SE -p builtin:free -t -1
 
 declare_test
-run_general t.in t.out t.err asserts/ls.sh -r SE -p builtin:free -m -128
+run_general t.in t.out /dev/null asserts/ls.sh -r SE -p builtin:free -m -128
 
 declare_test
-run_general t.in t.out t.err asserts/ls.sh -r SE -p builtin:free -g -128
+run_general t.in t.out /dev/null asserts/ls.sh -r SE -p builtin:free -g -128
 
 declare_test
-run_general t.in t.out t.err asserts/noexec_script.sh -r ECE -p builtin:free
+run_general t.in t.out /dev/null asserts/noexec_script.sh -r ECE -p builtin:free
 
 declare_test #15
-run_general t.in t.out t.err asserts/ls.sh -r SE -t 1000 -m 128 -P asserts -p fre
+run_general t.in t.out /dev/null asserts/ls.sh -r SE -t 1000 -m 128 -P asserts -p fre
 
 declare_test
-run_general t.in t.out t.err asserts/ls.sh -r SE -P asserts -p invalid -t 1000 -m 128
+run_general t.in t.out /dev/null asserts/ls.sh -r SE -P asserts -p invalid -t 1000 -m 128
 
 declare_test
-run_general t.in t.out t.err asserts/run_judger.sh -r ECE -p builtin:free -t 1000 -m 128
+run_general t.in t.out /dev/null asserts/run_judger.sh -r ECE -p builtin:free -t 1000 -m 128
 
 declare_test
 run_interactive guess.local interactor.local asserts/interactor.in \
-  t.out t.err t2.err -r OK -p builtin:free -t 0
+  t.out /dev/null /dev/null -r OK -p builtin:free -t 0
 
 declare_test
 run_interactive exit_1.local interactor.local asserts/interactor.in \
-  t.out t.err t2.err -r TLE -p builtin:free -t 600
+  t.out /dev/null /dev/null -r TLE -p builtin:free -t 600
 
 declare_test #20
 run_interactive guess.local interactor.local asserts/interactor.in \
-  t.out i.err t2.err -r SE -p builtin:free -t 1000
+  t.out i.err /dev/null -r SE -p builtin:free -t 1000
 
 declare_test
 run_interactive m.local interactor.local asserts/interactor.in \
-  t.out t.err t2.err -r TLE -p builtin:free -t 1000
+  t.out /dev/null /dev/null -r TLE -p builtin:free -t 1000
 
 declare_test
 run_interactive guess.local i.local asserts/interactor.in \
-  t.out t.err t2.err -r ECE -p builtin:free -t 1000
+  t.out /dev/null /dev/null -r ECE -p builtin:free -t 1000
 
 declare_test
 run_interactive guess.local i.local asserts/interactor.in \
-  t.out t.err t2.err -r SE -p builtin:free -t 1000 -m -1
+  t.out /dev/null /dev/null -r SE -p builtin:free -t 1000 -m -1
 
 declare_test
 run_interactive guess.local interactor.local asserts/interactor.in \
-  t.out t.err m.err -r SE -p builtin:free -t 1000
+  t.out /dev/null m.err -r SE -p builtin:free -t 1000
 
 declare_test #25
 run_interactive guess.local interactor.local asserts/interactor.in \
-  t.out t.err -r SE -p builtin:free -t 1000
+  t.out /dev/null -r SE -p builtin:free -t 1000
 
 declare_test
 run_interactive guess.local interactor.local asserts/interactor.in \
-  t.out t.err t2.err -r SE -p builtin:free -t 1000 --real-memory=-1
+  t.out /dev/null /dev/null -r SE -p builtin:free -t 1000 --real-memory=-1
 
 declare_test
 run_interactive guess.local interactor.local asserts/interactor.in \
-  t.out t.err t2.err -r SE -p builtin:free -t 1000 --stack-memory=-1
+  t.out /dev/null /dev/null -r SE -p builtin:free -t 1000 --stack-memory=-1
 
 declare_test "stress"
 
-run_traditional stress.local asserts/stress.in t.out t.err std \
+run_traditional stress.local asserts/stress.in t.out /dev/null std \
   -r OK -p builtin:_coverage --memory=128 --timeout=10000
 
 declare_test "stress RE"
-run_traditional stress.local asserts/stress.in t.out t.err std \
+run_traditional stress.local asserts/stress.in t.out /dev/null std \
   -r RE -p builtin:free --memory=30 --timeout=10000
 
 declare_test "real time TLE" #30
-run_traditional sleep_1_7.local asserts/aplusb.in t.out t.err std \
+run_traditional sleep_1_7.local asserts/aplusb.in t.out /dev/null std \
   -r TLE -p builtin:free --memory=30 --timeout=1500
 
 declare_test "CPU time TLE"
-run_traditional cpu_1_7.local asserts/aplusb.in t.out t.err std \
+run_traditional cpu_1_7.local asserts/aplusb.in t.out /dev/null std \
   -r TLE -p builtin:free --memory=30 --realtime=6000 --cputime=1500
 
 declare_test "segment fault"
-run_traditional sigsegv.local asserts/aplusb.in t.out t.err std \
+run_traditional sigsegv.local asserts/aplusb.in t.out /dev/null std \
   -r RE -p builtin:free -t 1000
 
 declare_test
-run_traditional sigusr1.local asserts/aplusb.in t.out t.err std \
+run_traditional sigusr1.local asserts/aplusb.in t.out /dev/null std \
   -r SE -p builtin:free -t 1000
 
 declare_test
-run_traditional aplusb.local asserts/aplusb.in t.out t.err std \
+run_traditional aplusb.local asserts/aplusb.in t.out /dev/null std \
   -r SE -P asserts -p invalid -t 1000
 
 declare_test "prlimit" #35
-run_traditional prlimit.local asserts/aplusb.in t.out t.err std \
-  -r ECE -p builtin:_coverage -t 1000
+run_general asserts/aplusb.in t.out /dev/null prlimit.local \
+  -r ECE -p builtin:_coverage_general_strict -t 1000
 
 declare_test
-run_general t.in t.out t.err -r SE -p builtin:free -t 1000 -m 128
+run_general t.in t.out /dev/null -r SE -p builtin:free -t 1000 -m 128
 
 declare_test
 run_general t.in t.out no.err asserts/ls.sh -r SE -P asserts -p free -t 1000
 
 declare_test
-run_general t.in no.out t.err asserts/ls.sh -r SE -P asserts -p free -t 1000
+run_general t.in no.out /dev/null asserts/ls.sh -r SE -P asserts -p free -t 1000
 
 declare_test
-run_general no.in t.out t.err asserts/ls.sh -r SE -P asserts -p free -t 1000
-
-declare_test #40
-run_traditional 03_tle.local asserts/aplusb.in t.out t.err std \
-	-r TLE -m 30 -t 1000 -g 64 -p builtin:_coverage
+run_general no.in t.out /dev/null asserts/ls.sh -r SE -P asserts -p free -t 1000
 
 test_finish
